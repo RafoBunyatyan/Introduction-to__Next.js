22364f6771f6775f1b7a777e04e12f88c6b46614
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-10-15 15:57:58 +0400

    add Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Fragment } from 'react'
 
 function NewsPage() {
@@ -6,9 +7,9 @@ function NewsPage() {
 			<h1>The News Page</h1>
 			<ul>
 				<li>
-					<a href='/news/nexj.js-is-a-great-framew0rd'>
+					<Link href='/news/nexj.js-is-a-great-framework'>
 						NextJS Is A Great Framework
-					</a>
+					</Link>
 				</li>
 				<li>Someting Else</li>
 			</ul>
