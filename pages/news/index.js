dbb66202d30c942ca2f0a0eef3cc36bfc6ebfa81
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-10-15 15:49:18 +0400

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
-// our-domain.com/news
+import { Fragment } from 'react'
 
 function NewsPage() {
-	return <h1>The News Page</h1>
+	return (
+		<Fragment>
+			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<a href='/news/nexj.js-is-a-great-framew0rd'>
+						NextJS Is A Great Framework
+					</a>
+				</li>
+				<li>Someting Else</li>
+			</ul>
+		</Fragment>
+	)
 }
 
 export default NewsPage
